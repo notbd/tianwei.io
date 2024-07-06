1e13f97cbfd939666db65c630147d4070904e734
TIANWEI ZHANG <git@tianwei.io>
2024-07-06 03:00:50 -0400

    feat: init draft

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,45 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Tianwei Zhang",
-  description: "A software developer.",
-};
+  title: 'Tianwei Zhang',
+  description: 'A software developer.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={inter.className}
+      >
+        {/* canvas */}
+        <div
+          className="flex min-h-screen flex-col items-center justify-between antialiased"
+        >
+
+          {/* top */}
+          <div>
+
+            {/* nav */}
+            <nav />
+
+            {/* main */}
+            <main>
+              {children}
+            </main>
+          </div>
+
+          {/* footer */}
+          <footer />
+        </div>
+      </body>
     </html>
-  );
+  )
 }
